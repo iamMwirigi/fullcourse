cb5694e71b1d50325f2583d29caf05a41c027aec
iamMwirigi <mwirigimburugu81@gmail.com>
2024-05-25 13:47:46 +0300

    done

--- base/views.py
+++ base/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
-from  django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    features = Feature.objects.all()  # Retrieve all features from the database
+    return render(request, 'index.html', {'features': features})  # Pass features to the template
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+                messages.info(request, 'User created successfully')
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
